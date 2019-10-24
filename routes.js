73d03fd30f8abe5d56560e57927ad7063b7d7b6a
Jill Glass <glass.nz@gmail.com>
2019-10-25 10:23:49 +1300

    index.js set up

---
+++ routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const server = express()
+
+module.exports = server
+
+// middleware
+
+// routes
+
+// server.get '/'
+
+// server.get '/list'
+
+// POST data coming back (read and write) - redirect over to /list
